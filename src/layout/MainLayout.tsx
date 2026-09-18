87e4b605d49ec13110825b9dafd29f07a1f3bd64
mzamoun <mourad.zamoun@gmail.com>
2026-09-18 13:21:32 +0200

    soc-39 Rendre paramétrable le fait qu une activité puisse etre ajoutée durant le weekend, jour ferié ou pas

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -120,7 +120,7 @@ export function MainLayout() {
   if (!user) return null
 
   const isAdmin = user.role === 'ADMIN'
-  const _LAST_COMMIT_ = '2026_09_18_11_32_45'
+  const _LAST_COMMIT_ = '2026_09_18_13_21_31'
 
   const sections: NavSection[] = [
     {
